dc87cf86e01862959ea281300617787bc5dc9322
NathanBarquira <75713895+NathanBarquira@users.noreply.github.com>
2021-06-27 21:44:44 -0700

    image idea?

---
+++ main.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+
+class Map:
+    def __init__(self):
+        self.board_setup()
+        self.create_map()
+        self.runUI()
+
+    def board_setup(self):
+        """ Creating the board """
+        self.master = tk.Tk()
+        self.master.title('Aim Trainer')
+        self.master.geometry('+{}+{}'.format(700, 300))
+        self.master.geometry('800x600')
+        self.master.configure(background='white')
+        self.master.resizable(0, 0)
+
+    def runUI(self):
+        """ run the UI """
+        self.master.mainloop()
+
+    def create_map(self):
+        image = tk.PhotoImage(file='map.jpeg')
+        self.map = tk.Label(self.master, image=image)
+        self.map.pack()
+
+
+if __name__ == '__main__':
+    game = Map()
